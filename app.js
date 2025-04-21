8c22e026e8b636a3a491898c4cf185fe9ea8ea6b
Yuri Viana <ycviana@sga.pucminas.br>
2025-04-21 08:35:30 -0300

    Projeto inicial

---
+++ app.js
@@ -0,0 +1,7 @@
+const inputItem = document.getElementById( 'input-item');
+const item = document.getElementById( 'lista-item' );
+
+const salvarInfo = document.getElementById( 'adicionar-item' );
+salvarInfo.addEventListener( 'click', (evento) => {
+    evento.preventDefault();
+})
